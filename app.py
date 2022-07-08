cb2333361e9e312292544b47da17d6260239558d
vicki <vickimathurin94@gmail.com>
2022-07-08 14:24:52 -0300

    conexion DB

--- app.py
+++ app.py
@@ -1,3 +1,32 @@
-from flask import Flask
 
-print('Hola mundo!')
+from flask import Flask
+from flask import render_template
+from flaskext.mysql import MySQL
+
+
+mysql=MySQL()
+
+app = Flask(__name__)
+mysql = MySQL()
+app.config['MYSQL_DATABASE_HOST']='localhost'
+app.config['MYSQL_DATABASE_USER']='root'
+app.config['MYSQL_DATABASE_PASSWORD']=''
+app.config['MYSQL_DATABASE_BD']='gestion_empresa'
+mysql.init_app(app)
+
+@app.route('/')
+def index():
+    sql = ""
+    conn = mysql.connect()
+    cursor = conn.cursor()
+    cursor.execute(sql)
+    conn.commit()
+
+    return render_template('proveedores/index.html')
+
+@app.route('/create')
+def create():
+ return render_template('proveedores/create.html')
+
+if __name__=='__main__':
+ app.run(debug=True)
